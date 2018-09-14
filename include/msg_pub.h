8ebaca4315e8cb4818fa3237e49c4da02b4a30ef
guolei <guolei@zerozero.cn>
2018-09-14 19:25:06 +0800

    update

--- include/msg_pub.h
+++ include/msg_pub.h
@@ -6,8 +6,11 @@
 #include <nn.h>
 #include <pubsub.h>
 #include <pipeline.h>
+#include <iostream>
+#include <google/protobuf/message.h>
 
 void fatal(const char *func);
 
 int msg_pub(const char *d, int sz_d);
 int msg_pub(const char *d);
+int msg_pub(const char * d, google::protobuf::Message &msg);
--- src/msg_pub/msg_pub.cpp
+++ src/msg_pub/msg_pub.cpp
@@ -10,20 +10,56 @@
 #include <pubsub.h>
 #include <pipeline.h>
 
-#define SERVER "server"
-#define NANOPUB "nanopub"
-#define CLIENT "client"
-#define ROUTER "router"
-#define SERVER_ADDR "ipc:///tmp/pubsub_server.ipc"
-#define CLIENT_ADDR "ipc:///tmp/pubsub_client.ipc"
+
 #define ROUTER_ADDR "tcp://127.0.0.1:19001"
 
-
+using namespace std;
+//std::string topiclist[];
 
 void fatal(const char *func)
 {
-        fprintf(stderr, "%s: %s\n", func, nn_strerror(nn_errno()));
+    fprintf(stderr, "%s: %s\n", func, nn_strerror(nn_errno()));
 }
+
+int msg_pub(const char * d, google::protobuf::Message &msg){
+	
+    int bytes;
+       
+    int sock;
+    if ((sock = nn_socket(AF_SP, NN_PUB)) < 0) 
+    {
+        fatal("nn_socket");
+    }
+    if (nn_connect(sock, ROUTER_ADDR) < 0) 
+    {
+        fatal("nn_connect");
+    }
+    //printf("msgpub ok1\n");
+    for (;;) 
+    {
+        char buf[4096];
+        int sz_d = strlen(d);
+        char protodata[4048];
+        //printf("msgpub ok2\n");
+        msg.SerializeToArray(protodata, 4048);
+        //printf("msgpub ok3\n");
+        sprintf(buf, "%s%c%s", d, '|', protodata);
+        //printf("msgpub ok4\n");
+        int len = sz_d + msg.ByteSize() + 1;
+        printf("sz_d is %d, msg.ByteSize() is %d, len is %d.\n", sz_d, msg.ByteSize(), len);
+         
+        printf("SERVER: PUBLISHING %s\n",d);
+        int bytes = nn_send(sock, buf, len, 0);
+        if (bytes < 0)
+        {
+            fatal("nn_send");
+        }
+                sleep(1);
+    }
+    return nn_shutdown(sock, 0);
+}
+
+
 
 int msg_pub(const char *d, int sz_d)
 {    
@@ -42,6 +78,8 @@ int msg_pub(const char *d, int sz_d)
         
         for (;;) 
         {
+            
+            /*
             int i = 0;
             int len_d = strlen(d);
             for(i = 0; i < len_d; i++)
@@ -54,12 +92,12 @@ int msg_pub(const char *d, int sz_d)
             
             int len = sz_d + i + 1;
              // '\0' too
-            
+            */
             printf("SERVER: PUBLISHING %s\n",d);
-            int bytes = nn_send(sock, d, len, 0);
+            int bytes = nn_send(sock, d, sz_d, 0);
             if (bytes < 0) 
             {
-                    fatal("nn_send");
+                fatal("nn_send");
             }
             //sleep(1);
         }
@@ -74,11 +112,11 @@ int msg_pub(const char *d)
         int sock;
         if ((sock = nn_socket(AF_SP, NN_PUB)) < 0) 
         {
-                fatal("nn_socket");
+            fatal("nn_socket");
         }
         if (nn_connect(sock, ROUTER_ADDR) < 0) 
         {
-                fatal("nn_connect");
+            fatal("nn_connect");
         }
         
         for (;;) 
@@ -88,7 +126,7 @@ int msg_pub(const char *d)
             int bytes = nn_send(sock, d, sz_d, 0);
             if (bytes < 0) 
             {
-                    fatal("nn_send");
+                fatal("nn_send");
             }
             //sleep(1);
         }
